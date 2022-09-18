babb5dd79c755fc7a1f7c673a35e4b5dee1d6d86
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-18 13:31:57 -0300

    feat: add Product Factory

---
+++ src/factories/product.factory.ts
@@ -0,0 +1,12 @@
+import { BaseControllerAbstract } from '../controllers/base.controller'
+import { ProductController } from '../controllers/product.controller'
+import { MongoProductRepository } from '../repositories/product.repository'
+import { ProductService } from '../services/product.service'
+
+const makeProductController = (): BaseControllerAbstract => {
+  const productRepository = new MongoProductRepository()
+  const productService = new ProductService(productRepository)
+  return new ProductController(productService)
+}
+
+export default makeProductController
