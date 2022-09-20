3c96e3d7c379cc44b9e410b596e125c4f419cb3a
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-20 15:05:36 -0300

    feat: add price to CreateProductDto and UpdateProductDto

--- src/dtos/product.dtos.ts
+++ src/dtos/product.dtos.ts
@@ -1,11 +1,13 @@
 export interface CreateProductDto {
   name: string
+  price: number
   imageUrl: string
   category: string
 } // contém todos os campos que devem ser necessariamente enviados para criar um produto
 
 export interface UpdateProductDto {
   name?: string
+  price?: number
   imageUrl?: string
   category?: string
 } // contém todos os campos que devem ser necessariamente enviados para atualizar um produto
