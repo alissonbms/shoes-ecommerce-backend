1c246e4a92ad0e5b507e0c6647cd22d009f65b60
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-20 15:37:31 -0300

    fix: disable eslint rule that was 'crashing' adaptRoute execution. So the express routes didn't work

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,17 +1,18 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import adaptRoute from '../adapters/express-route-adapter'
 import makeProductController from '../factories/product.factory'
 
 const router = Router()
 
-router.get('/', () => adaptRoute(makeProductController(), 'getAll'))
+router.get('/', adaptRoute(makeProductController(), 'getAll'))
 
-router.get('/:id', () => adaptRoute(makeProductController(), 'getOne'))
+router.get('/:id', adaptRoute(makeProductController(), 'getOne'))
 
-router.post('/', () => adaptRoute(makeProductController(), 'create'))
+router.post('/', adaptRoute(makeProductController(), 'create'))
 
-router.patch('/:id', () => adaptRoute(makeProductController(), 'update'))
+router.patch('/:id', adaptRoute(makeProductController(), 'update'))
 
-router.delete('/:id', () => adaptRoute(makeProductController(), 'delete'))
+router.delete('/:id', adaptRoute(makeProductController(), 'delete'))
 
 export default router
