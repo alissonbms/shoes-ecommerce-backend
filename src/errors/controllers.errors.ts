54c0fdc5233a3fd1d701423db0617d5a3dcd43de
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-22 14:57:50 -0300

    feat: add 'InvalidIdError' and 'SomethingWrong' errors in 'Controllers Errors'

--- src/errors/controllers.errors.ts
+++ src/errors/controllers.errors.ts
@@ -1,17 +1,27 @@
 export class MissingFieldError extends Error {
   constructor(field: string) {
-    super(`Missing ${field}`)
+    super(`Missing ${field}.`)
   }
 }
 
 export class MissingParamError extends Error {
   constructor(param: string) {
-    super(`Missing ${param} param`)
+    super(`Missing ${param} param.`)
   }
 }
-
+export class InvalidIdError extends Error {
+  constructor(property: string) {
+    super(`Invalid ${property} id`)
+  }
+}
 export class UpdateNotAllowedError extends Error {
   constructor() {
     super('Some received field is not allowed to update.')
+  }
+}
+
+export class SomethingWrong extends Error {
+  constructor(message: string) {
+    super(`${message} or something else went wrong. Try again later.`)
   }
 }
